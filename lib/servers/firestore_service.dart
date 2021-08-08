2d7565de01600c29f9ffb008c289a23a8f2f495e
henry mettle <mettlehenry573@gmail.com>
2021-08-07 18:01:58 -0700

    panel 1

---
+++ lib/servers/firestore_service.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:nm/models/washing_but.dart';
+
+Future<void> userSetup(String name, String email) async {
+  CollectionReference users = FirebaseFirestore.instance.collection('Users');
+  FirebaseAuth auth = FirebaseAuth.instance;
+  String uid = auth.currentUser.uid.toString();
+  users.add({'name': name, 'email': email, 'uid': uid});
+  return;
+}
+
+Future<void> infoUserSetup(
+    String name, String phone, String location, String carTyp,String washingBayName/*//TODO Add  this*/ ) async {
+  CollectionReference users = FirebaseFirestore.instance.collection('request');
+  FirebaseAuth auth = FirebaseAuth.instance;
+  String uid = auth.currentUser.uid.toString();
+  users.add({
+    'name': name,
+    'phone': phone,
+    'location': location,
+    'carTyp': carTyp,
+
+    //TODO Add  this
+    'washingBayName': washingBayName,
+
+    'timestamp': FieldValue.serverTimestamp()
+  });
+  return;
+}
+
+class FirestoreService {
+  FirebaseFirestore _db = FirebaseFirestore.instance;
+  Stream<List<WashingData>> fetchUpcomingWashingBay() {
+    return _db
+        .collection('washingbay')
+        .snapshots()
+        .map((query) => query.docs)
+        .map((snapshot) => snapshot
+            .map((doc) => WashingData.fromFirestore(doc.data()))
+            .toList());
+  }
+}
